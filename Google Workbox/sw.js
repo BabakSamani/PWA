65b9a40b51c77829ccd9971abb20b545a66542d1
babaksmani <Babak.j.samani@gmail.com>
2019-06-04 21:11:56 -0400

    Applying google workbox to make a progressive web app.

---
+++ Google Workbox/sw.js
@@ -0,0 +1,9 @@
+
+//Cache all the assets that we have for this app 
+const staticAssets = [
+    './', 
+    './src/css/styling.css', 
+    './src/js/scripts.js',
+    './src/images/fetch-failed.png',
+    './src/json/fallback.json'
+];
