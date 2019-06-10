fc79d287b8f00eb1a239f7b6d570b6eab61e62e7
babak <babak.j.samani@gmail.com>
2019-06-09 22:50:37 -0400

    Modifications and improvement on Google workbox Service worker.

--- Google Workbox/src/js/scripts.js
+++ Google Workbox/src/js/scripts.js
@@ -1,71 +1,85 @@
-
 const apiKey = "6e02eb5d1c3c42a6ad87aa2e635074df";
 
-const main = document.querySelector('main');
-const selectSource = document.querySelector('#sources');
-const defaultSource = 'bbc-news';
+const main = document.querySelector("main");
+const selectSource = document.querySelector("#sources");
+const defaultSource = "bbc-news";
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () =>
+    navigator.serviceWorker
+      .register("sw.js")
+      .then(registration => console.log("Service Worker registered"))
+      .catch(err => "SW registration failed")
+  );
+}
 
-window.addEventListener('load', async event => {
-
-    updateNews().then().catch(error => {
-        alert(`Errors in getting news: ${error}`);
+window.addEventListener("load", async event => {
+  updateNews()
+    .then()
+    .catch(error => {
+      alert(`Errors in getting news: ${error}`);
     });
 
-    await getSources().then().catch(error => {
-        alert(`Errors in getting sources: ${error}`);
+  await getSources()
+    .then()
+    .catch(error => {
+      alert(`Errors in getting sources: ${error}`);
     });
 
-    selectSource.value = defaultSource;
-    selectSource.addEventListener('change', e => {
-
-        updateNews(e.target.value).then().catch( error => {
-            if(error.message === 'Failed to fetch'){
-                console.log("Failed to fetch the news!");
-            } else {
-                alert(`Errors in getting news: ${error.message}`);
-            }
-
-        });
-
-    });
-
-    if('serviceWorker' in navigator){
-        try {
-            navigator.serviceWorker.register('sw.js');
-            console.log('Service worker is registered!');
-
-        } catch (error) {
-
-            console.log(`Service worker failed: ${error}`);
+  selectSource.value = defaultSource;
+  selectSource.addEventListener("change", e => {
+    updateNews(e.target.value)
+      .then()
+      .catch(error => {
+        if (error.message === "Failed to fetch") {
+          console.log("Failed to fetch the news!");
+        } else {
+          alert(`Errors in getting news: ${error.message}`);
         }
+      });
+  });
+
+  if ("serviceWorker" in navigator) {
+    try {
+      navigator.serviceWorker.register("sw.js");
+      console.log("Service worker is registered!");
+    } catch (error) {
+      console.log(`Service worker failed: ${error}`);
     }
+  }
 });
 
+window.addEventListener("online", () => updateNews(selectSource.value));
 
 async function getSources() {
-    const res = await fetch(`https://newsapi.org/v2/sources?language=en&apiKey=${apiKey}`);
-    const json = await res.json();
+  const res = await fetch(
+    `https://newsapi.org/v2/sources?language=en&apiKey=${apiKey}`
+  );
+  const json = await res.json();
 
-    selectSource.innerHTML = json.sources.map(src => `<option value="${src.id}">${src.name}</option>`).join('\n');
+  selectSource.innerHTML = json.sources
+    .map(src => `<option value="${src.id}">${src.name}</option>`)
+    .join("\n");
 }
 
 async function updateNews(source = defaultSource) {
-    let topic = 'apple';
+  let topic = "apple";
 
-    const res = await fetch(`https://newsapi.org/v2/everything?sources=${source}&q=${topic}&from=2019-06-01&to=2019-06-03&sortBy=popularity&apiKey=${apiKey}`);
-    const json = await res.json();
+  const res = await fetch(
+    `https://newsapi.org/v2/everything?sources=${source}&q=${topic}&from=2019-06-01&to=2019-06-03&sortBy=popularity&apiKey=${apiKey}`
+  );
+  const json = await res.json();
 
-    main.innerHTML = json.articles.map(createArticles).join('\n');
-
+  main.innerHTML = json.articles.map(createArticles).join("\n");
 }
 
 function createArticles(article) {
-    return `
+  return `
     <div class="article">
         <a href="${article.url}" target="_blank">
             <h2>${article.title}</h2>
-            <img src="${article.urlToImage || './src/images/img-not-found.png'}" alt="${article.source.name}">
+            <img src="${article.urlToImage ||
+              "./src/images/img-not-found.png"}" alt="${article.source.name}">
             <p>${article.description || " Description is not avaialble!"}</p>
         </a>
     </div>`;
--- Google Workbox/sw.js
+++ Google Workbox/sw.js
@@ -1,9 +1,66 @@
+// importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("./src/js/workbox-sw.js");
 
-//Cache all the assets that we have for this app 
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+
+const { strategies } = workbox;
+
+//Cache all the assets that we have for this app
 const staticAssets = [
-    './', 
-    './src/css/styling.css', 
-    './src/js/scripts.js',
-    './src/images/fetch-failed.png',
-    './src/json/fallback.json'
+  "./",
+  "./src/css/styling.css",
+  "./src/js/scripts.js",
+  "./src/images/fetch-failed.png",
+  "./src/json/fallback.json"
 ];
+
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  const url = new URL(req.url);
+
+  const cacheFirst = new workbox.strategies.CacheFirst();
+  const networkFirst = new workbox.strategies.NetworkFirst();
+
+  if( url.origin === location.origin ){
+    event.respondWith(cacheFirst.makeRequest({request: event.request}));
+  } else {
+    event.respondWith(networkFirst.makeRequest({request: event.request}));
+  }
+
+});
+
+// workbox.routing.registerRoute(
+//     // Cache CSS files.
+//     /\.css$/,
+//     // Use cache but update in the background.
+//     new workbox.strategies.StaleWhileRevalidate({
+//       // Use a custom cache name.
+//       cacheName: 'css-cache',
+//     })
+// );
+
+
+
+workbox.routing.registerRoute(
+    // Cache image files.
+    /\.(?:png|jpg|jpeg|svg|gif)$/,
+    // Use the cache if it's available.
+    new workbox.strategies.CacheFirst({
+      // Use a custom cache name.
+      cacheName: 'image-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          // Cache only 20 images.
+          maxEntries: 20,
+          // Cache for a maximum of a week.
+          maxAgeSeconds: 7 * 24 * 60 * 60,
+        })
+      ],
+    })
+);
+
